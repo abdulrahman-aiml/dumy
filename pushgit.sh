71911d7c27a4b4568d47c175b0b6590c0c00a5a8
cyber2138 <786mohdamaan2000@gmail.com>
2025-05-08 18:33:18 +0530

    Remove pushgit.sh from repository

--- pushgit.sh
+++
@@ -1,9 +0,0 @@
-git remote -v 
-echo "origin verifed"
-git add .
-echo "files added"
-git commit -m "new files added"
-git branch -m main
-echo "branch set to main"
-git push origin main
-echo "push successful"
